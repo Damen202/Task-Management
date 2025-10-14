0d90e089791e9fdccf45125059af8dbe96416b21
Damen202 <babatundeogundare195@gmail.com>
2025-10-14 13:36:54 +0100

    Add requirements.txt and update settings

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -13,6 +13,11 @@ import os
 from pathlib import Path
 from datetime import timedelta
 import dj_database_url
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 
 # Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('django-insecure-g+0yw-h0*n6_=c2f7m62xxo49x&hm6$x91tgyw*ojy#260v=p9')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
