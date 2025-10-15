08f5801679ae9d88abe3ba407e407fd8238fe561
Damen202 <babatundeogundare195@gmail.com>
2025-10-16 00:52:25 +0100

    urls updated

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/tasks/', include('tasks.urls')),
+    path('', lambda request: HttpResponse("🚀 Task Manager API is running successfully!")),
+    path('api/', include('tasks.urls')), 
 ]
